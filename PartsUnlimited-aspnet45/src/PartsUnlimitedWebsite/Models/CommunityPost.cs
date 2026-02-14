4574a47af7f10a0e5cbde80c0eacb55edb39a5d5
alagiewk82-ops <adamlagiewka@hotmail.com>
2026-02-14 10:12:22 +0100

    PTX-3022: Changed class name

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CommunityPost.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CommunityPost.cs
@@ -4,7 +4,7 @@ namespace PartsUnlimited.Models
 {
 
     public class CommunityPost
-    {
+    {   //My second change
         public string Image { get; set; }
         public string Content { get; set; }
         public DateTime DatePosted { get; set; }
