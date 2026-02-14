260a1cb0c233f208d330522dc7bfe24120b18aee
alagiewk82-ops <adamlagiewka@hotmail.com>
2026-02-14 11:45:46 +0100

    PTX:3335: Added second method

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Order.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Order.cs
@@ -66,5 +66,7 @@ namespace PartsUnlimited.Models
         public List<OrderDetail> OrderDetails { get; set; }
 
         /* A skeleton of very smart method is here */
+        /* A code of very smart second methjod is here
+
     }
 }
